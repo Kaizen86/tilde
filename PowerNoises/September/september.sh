3bf53ec7858dbfb2e0f502610303a93e09267e74
Blattoid <danielbridgewater87@gmail.com>
2021-06-25 09:20:36 +0100

    Update september.sh
    Removes device specification option as it will cause a crash later.

--- PowerNoises/September/september.sh
+++ PowerNoises/September/september.sh
@@ -6,6 +6,6 @@ date=$(date "+%m %d") #Get current date
 #Check if the date is September 21st
 if [ "$date" == "09 21" ]; then
 	#If it is, turn the speakers on and play a particular music track
-	amixer -D pulse sset Master 50\%;
+	amixer sset Master 50\%;
 	aplay $(dirname "$0")/September.wav
 fi
