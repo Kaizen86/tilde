973db6347ddadef6e6c36c6accd10e4ad98ec4e2
Blattoid <danielbridgewater87@gmail.com>
2021-07-13 01:15:14 +0100

    Minor tweaks

--- PowerNoises/September/september.sh
+++ PowerNoises/September/september.sh
@@ -7,5 +7,5 @@ date=$(date "+%m %d") #Get current date
 if [ "$date" == "09 21" ]; then
 	#If it is, turn the speakers on and play a particular music track
 	amixer sset Master 50\%;
-	aplay $(dirname "$0")/September.wav
+	play $(dirname "$0")/September.wav
 fi
